206cfff4059fd88d01c30e7e0976d7326199de40
warkoczek <53393030+warkoczek@users.noreply.github.com>
2022-02-13 20:10:52 +0100

    recipe-list component import added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import {FormsModule} from "@angular/forms";
 
 import {HeaderComponent} from "./header/header.component";
 import { RecipeComponent } from './recipe/recipe.component';
-// @ts-ignore
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import {RecipeListComponent} from "./recipe/recipe-list/recipe-list.component";
+
 
 
 
